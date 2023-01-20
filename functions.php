0d6f4effcd26a6dfdf34831c72bf1535d44958cf
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-01-20 13:31:16 +0100

    Download from WordCamp system

--- functions.php
+++ functions.php
@@ -1,6 +1,39 @@
 <?php
+/**
+ * Twenty Twenty-Two functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Two
+ * @since Twenty Twenty-Two 1.0
+ */
 
-if (!function_exists('wordcampswitzerland_2023_styles')) :
+
+if ( ! function_exists( 'twentytwentytwo_support' ) ) :
+
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
+	 *
+	 * @since Twenty Twenty-Two 1.0
+	 *
+	 * @return void
+	 */
+	function twentytwentytwo_support() {
+
+		// Add support for block styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style.css' );
+
+	}
+
+endif;
+
+add_action( 'after_setup_theme', 'twentytwentytwo_support' );
+
+if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 
 	/**
 	 * Enqueue styles.
@@ -9,28 +42,26 @@ if (!function_exists('wordcampswitzerland_2023_styles')) :
 	 *
 	 * @return void
 	 */
-	function wordcampswitzerland_2023_styles()
-	{
+	function twentytwentytwo_styles() {
 		// Register theme stylesheet.
-		$theme_version = wp_get_theme()->get('Version');
+		$theme_version = wp_get_theme()->get( 'Version' );
 
-		$version_string = is_string($theme_version) ? $theme_version : false;
+		$version_string = is_string( $theme_version ) ? $theme_version : false;
 		wp_register_style(
-			'wordcampswitzerland_2023-style',
-			get_stylesheet_directory_uri() . '/style.css',
+			'twentytwentytwo-style',
+			get_template_directory_uri() . '/style.css',
 			array(),
 			$version_string
 		);
 
 		// Enqueue theme stylesheet.
-		wp_enqueue_style('wordcampswitzerland_2023-style');
+		wp_enqueue_style( 'twentytwentytwo-style' );
+
 	}
 
-	add_action('wp_enqueue_scripts', 'wordcampswitzerland_2023_styles');
 endif;
 
-add_action('wp_enqueue_scripts', 'twentytwentytwo_styles');
+add_action( 'wp_enqueue_scripts', 'twentytwentytwo_styles' );
 
-
-// // Add block patterns
-// require get_template_directory() . '/inc/block-patterns.php';
+// Add block patterns
+require get_template_directory() . '/inc/block-patterns.php';
